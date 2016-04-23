d339878abcdbaa96b5bf68332a5f7d85cc60cc18
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2016-04-23 14:55:53 +0200

    Inital code commit

---
+++ ISYMMSettings.cs
@@ -0,0 +1,67 @@
+﻿namespace SYMM.Interfaces
+{
+    public enum AudioFormats
+    {
+        mp3,
+        wav,
+        m4a,
+        wma,
+        ogg
+    }
+
+    public enum Actions
+    {
+        Download,
+        ExtractAudio,
+        Stream
+    }
+
+    public enum Mode
+    {
+        Single,
+        All
+    }
+
+    public struct URLSpecs
+    {
+        public bool IsPlaylist;
+        public bool ContainsVideo;
+
+        public URLSpecs(string url)
+        {
+            URLSpecs specs = URLSpecs.ExamineURL(url);
+            this.IsPlaylist = specs.IsPlaylist;
+            this.ContainsVideo = specs.ContainsVideo;
+        }
+
+        public URLSpecs(bool IsPlaylist, bool ContainsVideo)
+        {
+            this.IsPlaylist = IsPlaylist;
+            this.ContainsVideo = ContainsVideo;
+        }
+
+        public static URLSpecs ExamineURL(string url)
+        {
+            return new URLSpecs(url.Contains("list="), url.Contains("watch?v="));
+        }
+    }
+
+    public interface ISYMMSettings
+    {
+        Actions Action { get; }
+        Mode DownloadMode { get; }
+        AudioFormats AudioFormat { get; }
+
+        URLSpecs UrlSpecs { get; }
+
+        bool CheckDuplicate { get; }
+
+        short VideoResolution { get; }
+        short AudioBitrate { get; }
+       
+        string DownloadURL { get; }
+        string PathSafefileName { get; set; }
+        string SavePath { get; }
+        string AlbumNameRegex { get; }
+    }
+}
---
+++ IYouTubeVideo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace SYMM.Interfaces
+{
+    public interface IYouTubeVideo
+    {
+        Bitmap GetThumbnail();
+        DateTime? PublishDate { get; }
+
+        long? PlayListPos { get; }
+        
+        string Desc { get; }
+        string GetWatchID();
+        string ThumbURL { get; }
+        string VideoTitle { get; }
+        string ChannelTitle { get; }
+        string VideoWatchID { get; }
+    }
+}
